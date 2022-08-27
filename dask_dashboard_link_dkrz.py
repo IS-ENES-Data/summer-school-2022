f9d949ec1e0874e883ba41093593304e8f3d5619
stephank16 <kindermann@dkrz.de>
2022-08-27 11:40:25 +0200

    cloud update

--- dask_dashboard_link_dkrz.py
+++
@@ -1,5 +0,0 @@
-# set the correct status link for dashboard
-import dask
-from dask.distributed import Client
-
-dask.config.config.get('distributed').get('dashboard').update({'link':'{JUPYTERHUB_SERVICE_PREFIX}/proxy/{port}/status'})
